402d7e2d260d761c61335fa5c5f4d6707f3c3c2d
ayykapu <kurkeu@gmail.com>
2024-01-20 18:35:24 +0100

    lista obiektów

---
+++ Post-MVC/Models/Post.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Post.Models
+{
+    public enum Tags
+    {
+        [Display(Name = "Nowość")]
+        New,
+        [Display(Name = "Technologia")]
+        Tech,
+        [Display(Name = "Sport")]
+        Sport,
+        [Display(Name = "Zdrowie")]
+        Health,
+        [Display(Name = "Polityka")]
+        Politics,
+    }
+
+    public class PostClass
+    {
+        [HiddenInput]
+        public int Id { get; set; }
+
+        [Display(Name = "Zawartość")]
+        [Required(ErrorMessage = "Musisz podać zawartość wpisu!")]
+        [StringLength(maximumLength: 150, ErrorMessage = "Post jest zbyt długi!")]
+        public required string Content { get; set; }
+
+        [Display(Name = "Autor wpisu")]
+        [Required(ErrorMessage = "Musisz podać nick autora wpisu!")]
+        [StringLength(maximumLength: 50, ErrorMessage = "Nick jest zbyt długi!")]
+        public required string Author { get; set; }
+
+        [DataType(DataType.Date)]
+        public required DateTime Date { get; set; }
+
+        [Display(Name = "Komentarz")]
+        [Required(ErrorMessage = "Komentarz jest wymagany!")]
+        public required string Comment { get; set; }
+
+        [Display(Name = "Tagi")]
+        [Required(ErrorMessage = "Musisz podać tag autora wpisu!")]
+        public required Tags Tags { get; set; }
+    }
+}
